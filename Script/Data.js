2cfad1c9e57940c42d1ae2f02c7d888da9ca6543
Pourya Zardosht <pooriyazardosht.812@gmail.com>
2023-08-17 16:50:43 +0330

    filter added

--- Script/Data.js
+++ Script/Data.js
@@ -1,13 +1,13 @@
 const cardBox = document.querySelector(".cardBoxContainer");
+const search = document.querySelector("#search");
+let productName = [];
 let products = [];
 axios.get("https://fakestoreapi.com/products").then((res) => {
   products = res.data;
-  console.log(products);
   showData();
 });
 showData = () => {
   products.forEach((product) => {
-    console.log(product);
     cardBox.innerHTML += `
     <div data-aos="flip-left" data-aos-duration="1000" class="myCard">
     <div class="imgBox">
@@ -18,5 +18,7 @@ showData = () => {
     <button>خرید</button>
   </div>
     `;
+    productName.push(product.title);
   });
 };
+
